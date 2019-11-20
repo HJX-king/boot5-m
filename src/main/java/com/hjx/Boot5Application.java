915488dcf30510f6df5b32f5d5a61bf30f7c9aae
weimac <48160227+weimac@users.noreply.github.com>
2019-11-20 11:44:30 +0800

    Update Boot5Application.java

--- src/main/java/com/hjx/Boot5Application.java
+++ src/main/java/com/hjx/Boot5Application.java
@@ -14,7 +14,7 @@ public class Boot5Application {
         System.out.println("-------------------");
         System.out.println("偷家!!!");
         System.out.println("-------------------");
-        System.out.println("啊啊啊啊");
+        System.out.println("偷家!!!");
 
     }
 
