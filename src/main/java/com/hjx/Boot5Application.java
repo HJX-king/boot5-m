e22f93c5dd2d6160bb462058e3c5b15c64528e44
hjx_global <1178814406@qq.com>
2019-11-20 10:28:42 +0800

    项目经理第二次次 提交项目boot5

--- src/main/java/com/hjx/Boot5Application.java
+++ src/main/java/com/hjx/Boot5Application.java
@@ -1,13 +1,16 @@
 package com.hjx;
 
+import com.hjx.controller.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Boot5Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Boot5Application.class, args);
+        System.out.println("快点给我写代码!!!!!");
     }
 
 }
---
+++ src/main/java/com/hjx/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.hjx.controller;
+
+/**
+ * @Description:
+ * @Company:
+ * @Auther:xin
+ * @Date:2019/11/20
+ * @Time:10:25
+ */
+public class HelloController {
+    public void t(){
+        System.out.println("快点给我写代码!!!!!");
+    }
+}
