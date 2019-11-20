1876b7040065bacf1afe2e49024762f6fd3f2022
weixin_global <48160227+weimac@users.noreply.github.com>
2019-11-20 11:14:45 +0800

    开发人员第二次修改

--- src/main/java/com/hjx/Boot5Application.java
+++ src/main/java/com/hjx/Boot5Application.java
@@ -10,7 +10,7 @@ public class Boot5Application {
     public static void main(String[] args)
     {
         SpringApplication.run(Boot5Application.class, args);
-        System.out.println("快点给我写代码!!!!!");
+        System.out.println("快点给我写代码!!!!!1111111");
         System.out.println("-------------------");
         System.out.println("偷家!!!");
         System.out.println("-------------------");
